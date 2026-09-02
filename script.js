38a2988c10661bbdd78b43dbfa71b5fd96b16994
Luiz Felipe de Cesaro Rorigues <luizfelipedecesarorodrigues@gmail.com>
2026-09-02 17:11:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -75,3 +75,70 @@ function verificarPrioridade() {
             resultado.textContent = "Prioridade Desconhecida.";
     }
 }
+
+
+// ==================================
+//         Atividade 5
+// ==================================
+
+
+function verificarTarefa(){
+    const prioridade = document.getElementById("prioridadeT").value;
+    const resultado = document.getElementById("resultados");
+    const atrasada = document.getElementById("atrasadaT").checked;
+ 
+resultado.classList.remove("alerta", "calmo");
+ 
+    if(prioridade === "selecionarT"){
+        resultado.textContent = "A Prioridade é desconhecida";
+        resultado.classList.add("mostrar", "desconhecida")
+    }
+    else if(prioridade === "urgenteT" || (prioridade === "altaT" && atrasada)){
+        resultado.textContent = "Precisa de atenção agora!";
+        resultado.classList.add("mostrar", "alerta");
+    }
+    else{
+        resultado.textContent = "Pode aguardar";
+        resultado.classList.add("mostrar", "calmo");
+    }
+}
+
+//=================================
+// atividade 6
+// ================================
+
+function verificarLogin() {
+ 
+    const usuario = document.getElementById("usuario").value;
+
+    const senha = document.getElementById("senha").value;
+ 
+    if (usuario === "admin" && senha === "1234") {
+
+        document.getElementById("ResultadoLogin").textContent = "Login realizado com sucesso!";
+
+    } else if (usuario === "admin" && senha !== "1234") {
+
+        document.getElementById("ResultadoLogin").textContent = "Senha incorreta.";
+
+    } else {
+
+        document.getElementById("ResultadoLogin").textContent = "Usuário não encontrado.";
+
+    }
+ 
+}
+ 
+//=================================
+// atividade 7
+// ================================
+
+function verificarIdadeTernario() {
+ 
+    const idade = Number(document.getElementById("idadeTernario").value);
+ 
+    const situacao = idade >= 18 ? "Maior de idade" : "Menor de idade";
+ 
+    document.getElementById("ResultadoTernario").textContent = situacao;
+    
+}
